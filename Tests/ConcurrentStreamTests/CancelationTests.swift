7a8d2b9670753b99ddbd9b1d03c38d1c6e2fe9d1
Vaida <developer@vaidaf.net>
2024-06-14 15:34:39 +1000

    CI for release tests

--- Tests/ConcurrentStreamTests/CancelationTests.swift
+++ Tests/ConcurrentStreamTests/CancelationTests.swift
@@ -8,10 +8,11 @@ import Synchronization
 import Testing
 
 @Suite("Cancelation Tests", .tags(.cancelation))
-struct CanceltaionTests {
+struct CancellationTests {
     
     @available(macOS 15.0, *)
-    func taskCancelation() async throws {
+    @Test
+    func taskCancellation() async throws {
         let counter = Atomic<Int>(0)
         let task = Task.detached {
             let stream = await (1...100).stream.map { _ in
@@ -25,6 +26,49 @@ struct CanceltaionTests {
         heavyJob() // call it twice to ensure stream actually runs.
         
         task.cancel()
+        try! await Task.sleep(for: .seconds(10)) //ensures stream is completed, even when task cancelation is faulty.
+        
+        try #require(counter.load(ordering: .acquiring) > 0, "The stream should have been executed for at least one time, please adjust conditions before calling task.cancel")
+        #expect(counter.load(ordering: .acquiring) < 50)
+    }
+    
+    // stream is released at once, should be blocked before the first child task was even created.
+    @available(macOS 15.0, *)
+    @Test
+    func releaseAtOnce() async throws {
+        let counter = Atomic<Int>(0)
+        let task = Task.detached {
+            let stream = await (1...100).stream.map { _ in
+                heavyJob()
+                counter.add(1, ordering: .relaxed)
+            }
+        }
+        
+        heavyJob()
+        heavyJob() // call it twice to ensure stream actually runs.
+        
+        try! await Task.sleep(for: .seconds(10)) //ensures stream is completed, even when task cancelation is faulty.
+        
+        #expect(counter.load(ordering: .acquiring) == 0)
+    }
+    
+    @available(macOS 15.0, *)
+    @Test
+    func releaseLater() async throws {
+        let counter = Atomic<Int>(0)
+        let task = Task.detached {
+            let stream = await (1...100).stream.map { _ in
+                heavyJob()
+                counter.add(1, ordering: .relaxed)
+            }
+            
+            heavyJob()
+            heavyJob() //ensures stream is not deallocated at once.
+        }
+        
+        heavyJob()
+        heavyJob() // call it twice to ensure stream actually runs.
+        
         try! await Task.sleep(for: .seconds(10)) //ensures stream is completed, even when task cancelation is faulty.
         
         try #require(counter.load(ordering: .acquiring) > 0, "The stream should have been executed for at least one time, please adjust conditions before calling task.cancel")
