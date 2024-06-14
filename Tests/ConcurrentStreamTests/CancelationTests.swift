63b51ed771d912241f7fa7ac7be7c60e70b5c69f
Vaida <developer@vaidaf.net>
2024-06-14 15:50:33 +1000

    Fixed Atomic in testing

--- Tests/ConcurrentStreamTests/CancelationTests.swift
+++ Tests/ConcurrentStreamTests/CancelationTests.swift
@@ -18,19 +18,19 @@ struct CancellationTests {
         let task = Task.detached {
             let stream = await (1...100).stream.map { _ in
                 heavyJob()
-                counter.add(1, ordering: .relaxed)
+                counter.add(1, ordering: .sequentiallyConsistent)
             }
-            try? await Task.sleep(for: .seconds(10)) //ensures stream is not deallocated at once.
+            try? await Task.sleep(for: .seconds(5)) //ensures stream is not deallocated at once.
         }
         
         heavyJob()
         heavyJob() // call it twice to ensure stream actually runs.
         
         task.cancel()
-        try! await Task.sleep(for: .seconds(10)) //ensures stream is completed, even when task cancelation is faulty.
+        try! await Task.sleep(for: .seconds(5)) //ensures stream is completed when task cancelation is faulty.
         
-        try #require(counter.load(ordering: .acquiring) > 0, "The stream should have been executed for at least one time, please adjust conditions before calling task.cancel")
-        #expect(counter.load(ordering: .acquiring) < 50)
+        try #require(counter.load(ordering: .sequentiallyConsistent) > 0, "The stream should have been executed for at least one time, please adjust conditions before calling task.cancel")
+        #expect(counter.load(ordering: .sequentiallyConsistent) < 50)
     }
     
     // stream is released at once, should be blocked before the first child task was even created.
@@ -41,16 +41,16 @@ struct CancellationTests {
         let task = Task.detached {
             let stream = await (1...100).stream.map { _ in
                 heavyJob()
-                counter.add(1, ordering: .relaxed)
+                counter.add(1, ordering: .sequentiallyConsistent)
             }
         }
         
         heavyJob()
         heavyJob() // call it twice to ensure stream actually runs.
         
-        try! await Task.sleep(for: .seconds(10)) //ensures stream is completed, even when task cancelation is faulty.
+        try! await Task.sleep(for: .seconds(5)) //ensures stream is completed when task cancelation is faulty.
         
-        #expect(counter.load(ordering: .acquiring) == 0)
+        #expect(counter.load(ordering: .sequentiallyConsistent) == 0)
     }
     
     @available(macOS 15.0, *)
@@ -60,7 +60,7 @@ struct CancellationTests {
         let task = Task.detached {
             let stream = await (1...100).stream.map { _ in
                 heavyJob()
-                counter.add(1, ordering: .relaxed)
+                counter.add(1, ordering: .sequentiallyConsistent)
             }
             
             heavyJob()
@@ -70,10 +70,10 @@ struct CancellationTests {
         heavyJob()
         heavyJob() // call it twice to ensure stream actually runs.
         
-        try! await Task.sleep(for: .seconds(10)) //ensures stream is completed, even when task cancelation is faulty.
+        try! await Task.sleep(for: .seconds(5)) //ensures stream is completed when task cancelation is faulty.
         
-        try #require(counter.load(ordering: .acquiring) > 0, "The stream should have been executed for at least one time, please adjust conditions before calling task.cancel")
-        #expect(counter.load(ordering: .acquiring) < 50)
+        try #require(counter.load(ordering: .sequentiallyConsistent) > 0, "The stream should have been executed for at least one time, please adjust conditions before calling task.cancel")
+        #expect(counter.load(ordering: .sequentiallyConsistent) < 50)
     }
     
     @available(macOS 15.0, *)
@@ -84,23 +84,26 @@ struct CancellationTests {
         let task = Task.detached {
             let stream = await (1...100).stream.map { _ in
                 heavyJob()
-                counter.add(1, ordering: .relaxed)
+                counter.add(1, ordering: .sequentiallyConsistent)
             }
             
             while let next = await stream.next() {
-                nextCounter.add(1, ordering: .relaxed)
+                nextCounter.add(1, ordering: .sequentiallyConsistent)
             }
         }
         
-        heavyJob()
-        heavyJob() // call it twice to ensure stream actually runs.
+        // ensure it actually runs.
+        while counter.load(ordering: .sequentiallyConsistent) == 0 || nextCounter.load(ordering: .sequentiallyConsistent) == 0 {
+            heavyJob()
+        }
         
         task.cancel()
-        try! await Task.sleep(for: .seconds(10)) //ensures stream is completed, even when task cancelation is faulty.
+        try! await Task.sleep(for: .seconds(5)) //ensures stream is completed, even when task cancelation is faulty.
         
-        try #require(counter.load(ordering: .acquiring) > 0, "The stream should have been executed for at least one time, please adjust conditions before calling task.cancel")
-        try #require(nextCounter.load(ordering: .acquiring) > 0 && nextCounter.load(ordering: .acquiring) < 10, "Please adjust time conditions according.")
-        #expect(counter.load(ordering: .acquiring) < 50)
+        print(nextCounter.load(ordering: .sequentiallyConsistent))
+        try #require(nextCounter.load(ordering: .sequentiallyConsistent) > 0, "Please adjust time conditions according.")
+        try #require(nextCounter.load(ordering: .sequentiallyConsistent) < 20, "Please adjust time conditions according.")
+        #expect(counter.load(ordering: .sequentiallyConsistent) < 20)
     }
     
 }
